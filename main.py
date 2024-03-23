c2de8750021a0bdeaa15251b19efb37e58109fe5
giuzepp <giuseppebianco11191@gmail.com>
2024-03-23 18:26:45 +0100

    secondo commit

--- main.py
+++ main.py
@@ -6,5 +6,7 @@ app = Flask(__name__)
 @app.route('/home')
 def index():
     return render_template('index.html')
+def catalogo():
+    return render_template('catalogo.html')
 
 app.run(host='0.0.0.0', port=80)
